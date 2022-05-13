7f9f5697363acf76687aa476984737939fbc967d
Steve Moritani <steve.hayato.m@gmail.com>
2022-05-12 18:40:43 -0700

    AnimatedDisplayCard

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} /> 
+        <Route path="/" element={<HomePage />} />
         <Route path="contact" element={<ContactPage />} />
         <Route path="directory" element={<CampsitesDirectoryPage />} />
-        <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+        <Route path="about" element={<AboutPage />} />
       </Routes>
       <Footer />
     </div>
